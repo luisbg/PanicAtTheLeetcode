133a302e68dcd26fb45bf14b1271332b2b8ba666
Luis de Bethencourt <luis@debethencourt.com>
2025-11-25 12:28:14 -0500

    quest 03: arrays: max consecutive (more idiomatic)

--- quest/arrays/03_max_consecutive.py
+++ quest/arrays/03_max_consecutive.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def findMaxConsecutiveOnes(self, nums):
+    def raw_findMaxConsecutiveOnes(self, nums):
         """
         :type nums: List[int]
         :rtype: int
@@ -13,6 +13,19 @@ class Solution(object):
                 tmp += 1
                 if tmp > ones:
                     ones = tmp
+            else:
+                tmp = 0
+
+        return ones
+
+    def findMaxConsecutiveOnes(self, nums):
+        ones = 0
+        tmp = 0
+
+        for x in nums:
+            if x == 1:
+                tmp += 1
+                ones = max(ones, tmp)
             else:
                 tmp = 0
 
