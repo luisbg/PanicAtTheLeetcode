93a114feba805787be9dce30c60743d45cf5c35e
Luis de Bethencourt <luis@debethencourt.com>
2025-11-26 16:37:11 -0500

    quest 09: stack: exclusive time of functions (slightly faster)

--- quest/stack/09_exclusive_time_of_functions.py
+++ quest/stack/09_exclusive_time_of_functions.py
@@ -10,18 +10,19 @@ class Solution(object):
         calls = []
         last_time = 0
         for log in logs:
-            fid, act, time = log.split(':')
+            fid_str, act, time_str = log.split(':')
+            time = int(time_str)
+            fid = int(fid_str)
 
             if calls:
                 curr = calls[-1]
-                times[curr] += int(time) - last_time
-
-            last_time = int(time)
+                times[curr] += time - last_time
 
             if act == "start":
-                calls.append(int(fid))
+                calls.append(fid)
+                last_time = time
             else:
                 times[calls.pop()] += 1 # need increment because time is inclusive
-                last_time += 1 # also due to inclusive time :(
+                last_time = time + 1 # also due to inclusive time :(
         
         return times
