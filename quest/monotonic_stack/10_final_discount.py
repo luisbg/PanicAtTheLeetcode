d53bbd3c60662ed634dbb306202a2b99cf4fc880
Luis de Bethencourt <luis@debethencourt.com>
2025-11-27 10:29:37 -0500

    quest 10: monotonic stack: final discount (monotonic stack)

--- quest/monotonic_stack/10_final_discount.py
+++ quest/monotonic_stack/10_final_discount.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def finalPrices(self, prices):
+    def bruteForcefinalPrices(self, prices):
         """
         :type prices: List[int]
         :rtype: List[int]
@@ -17,4 +17,33 @@ class Solution(object):
             if not discount:
                 ans.append(price)
         
+        return ans
+
+    def monotonicStackFinalPrices(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: List[int]
+        """
+        n = len(prices)
+        ans = [0] * n
+        stack = []
+
+        # walk from right to left
+        for i in range(n - 1, -1, -1):
+            price = prices[i]
+
+            # remove prices bigger than price
+            while stack and stack[-1] > price:
+                stack.pop()
+
+            # stack contains nearest price <= price
+            if stack:
+                discount = stack[-1]
+            else:
+                discount = 0
+            ans[i] = price - discount
+
+            # price is candidate discount for items to the left
+            stack.append(price)
+
         return ans
