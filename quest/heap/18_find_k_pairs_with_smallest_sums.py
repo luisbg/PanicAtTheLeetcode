a0053c9af51f7c3faa9cedfb6e0205c91add2e81
Luis de Bethencourt <luis@debethencourt.com>
2025-11-28 15:08:29 -0500

    quest 18: heap: find k pairs with smallest sums (efficient)

--- quest/heap/18_find_k_pairs_with_smallest_sums.py
+++ quest/heap/18_find_k_pairs_with_smallest_sums.py
@@ -27,3 +27,32 @@ class Solution(object):
             tmp = heapq.heappop(heap)
             ans.append([tmp[1], tmp[2]])
         return ans
+
+    def kSmallestPairs(self, nums1, nums2, k):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        if not nums1 or not nums2 or k == 0:
+            return []
+
+        n1, n2 = len(nums1), len(nums2)
+        heap = []
+        ans = []
+
+        # Seed heap with (nums1[i], nums2[0]) for i in [0, min(k, n1) )
+        for i in range(min(k, n1)):
+            # (sum, index in nums1, index in nums2)
+            heapq.heappush(heap, (nums1[i] + nums2[0], i, 0))
+
+        while heap and len(ans) < k:
+            s, i, j = heapq.heappop(heap)
+            ans.append([nums1[i], nums2[j]])
+
+            # Move to next in nums2 for this i
+            if j + 1 < n2:
+                heapq.heappush(heap, (nums1[i] + nums2[j + 1], i, j + 1))
+
+        return ans
