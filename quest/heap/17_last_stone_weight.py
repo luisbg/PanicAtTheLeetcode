69c6316820e5ebf8acb5b9f1e37a6527dbd5501e
Luis de Bethencourt <luis@debethencourt.com>
2025-11-28 14:26:34 -0500

    quest 17: heap: last stone weight

---
+++ quest/heap/17_last_stone_weight.py
@@ -0,0 +1,38 @@
+"""
+You are given an array of integers stones where stones[i] is the weight of the ith stone.
+
+We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together. Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+
+    If x == y, both stones are destroyed, and
+    If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+
+At the end of the game, there is at most one stone left.
+
+Return the weight of the last remaining stone. If there are no stones left, return 0.
+"""
+
+import heapq
+
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: int
+        """
+        maxHeap = []
+        for s in stones:
+            heapq.heappush(maxHeap, s * -1)
+
+        while len(maxHeap) > 1:
+            y = heapq.heappop(maxHeap) * -1
+            x = heapq.heappop(maxHeap) * -1
+
+            if x == y:
+                continue
+
+            heapq.heappush(maxHeap, (y - x) * - 1)
+
+        if len(maxHeap) == 1:
+            return heapq.heappop(maxHeap) * -1
+        else:
+            return 0
