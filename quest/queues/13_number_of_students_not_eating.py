d9e5e57166aede182da4bd4a99fe169b5c0103d3
Luis de Bethencourt <luis@debethencourt.com>
2025-11-28 10:37:55 -0500

    quest 13: queues: number of students unable to eat (with deque)

--- quest/queues/13_number_of_students_not_eating.py
+++ quest/queues/13_number_of_students_not_eating.py
@@ -10,6 +10,7 @@ This continues until none of the queue students want to take the top sandwich an
 
 You are given two integer arrays students and sandwiches where sandwiches[i] is the type of the i-th sandwich in the stack (i = 0 is the top of the stack) and students[j] is the preference of the j-th student in the initial queue (j = 0 is the front of the queue). Return the number of students that are unable to eat.
 """
+from collections import deque
 
 class Solution(object):
     def countStudents(self, students, sandwiches):
@@ -22,6 +23,7 @@ class Solution(object):
         tried = 0
         while students:
             if students[0] == sandwiches[0]:
+                # pop(0) in a list is O(n)
                 students.pop(0)
                 sandwiches.pop(0)
                 tried = 0
@@ -34,3 +36,33 @@ class Solution(object):
                 break
         
         return len(students)
+
+    def queueCountStudents(self, students, sandwiches):
+        """
+        :type students: List[int]
+        :type sandwiches: List[int]
+        :rtype: int
+        """
+
+        st_q = deque()
+        for s in students:
+            st_q.append(s)
+
+        tried = 0
+        students_left = len(st_q)
+        while st_q:
+            if st_q[0] == sandwiches[0]:
+                # popleft() in a dequeue is O(1)
+                st_q.popleft()
+                sandwiches.pop(0)
+                tried = 0
+                students_left -= 1
+            else:
+                s = st_q.popleft()
+                st_q.append(s)
+                tried += 1
+
+            if tried == students_left:
+                break
+
+        return students_left
