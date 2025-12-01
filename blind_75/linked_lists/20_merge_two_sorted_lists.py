da16c3b7759ec9a2e0baf4bb63490f696c6d15d1
Luis de Bethencourt <luis@debethencourt.com>
2025-12-01 12:52:54 -0500

    blind 75: 09: linked lists: merge two sorted lists (use existing nodes)

--- blind_75/linked_lists/20_merge_two_sorted_lists.py
+++ blind_75/linked_lists/20_merge_two_sorted_lists.py
@@ -50,3 +50,29 @@ class Solution(object):
             head = curr1 if curr1 else curr2
 
         return head
+
+    def useExistingMergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode()      # dummy head to simplify edge cases
+        tail = dummy            # tail always points to last node in merged list
+
+        p1, p2 = list1, list2
+
+        # Walk both lists while they both have nodes
+        while p1 and p2:
+            if p1.val <= p2.val:
+                tail.next = p1
+                p1 = p1.next
+            else:
+                tail.next = p2
+                p2 = p2.next
+            tail = tail.next
+
+        # At most one of p1, p2 is non-null now; append the remainder
+        tail.next = p1 if p1 else p2
+
+        return dummy.next
