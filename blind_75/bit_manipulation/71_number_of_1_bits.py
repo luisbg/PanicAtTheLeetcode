c11ddcb3c6a275480377ddf1389451ec949d87bb
Luis de Bethencourt <luis@debethencourt.com>
2025-12-01 16:22:11 -0500

    blind 75: 14: bit manipulation: number of 1 bits (cleaner)

--- blind_75/bit_manipulation/71_number_of_1_bits.py
+++ blind_75/bit_manipulation/71_number_of_1_bits.py
@@ -12,11 +12,18 @@ class Solution(object):
 
         # until the number has no 1 bits
         while n > 0:
-            # check if lsb is a 1
-            if n % 2 == 1:
+            # check if LSB is a 1
+            if n & 1:
                 count += 1
 
             # shift right by 1
             n = n >> 1
 
+        return count
+
+    def KernighanHammingWeight(self, n):
+        count = 0
+        while n:
+            n &= n - 1   # clear the lowest set bit
+            count += 1
         return count
