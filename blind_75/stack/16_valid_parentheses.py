8418ff725b130438dd5b8a513742987346d63cc9
Luis de Bethencourt <luis@debethencourt.com>
2025-12-01 10:36:50 -0500

    blind 75: 07: stack: valid parentheses

---
+++ blind_75/stack/16_valid_parentheses.py
@@ -0,0 +1,42 @@
+"""
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+    Open brackets must be closed by the same type of brackets.
+    Open brackets must be closed in the correct order.
+    Every close bracket has a corresponding open bracket of the same type.
+"""
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # one stacks for (), {}, and []
+        stack = []
+
+        # run through the string
+        for b in s:
+            # when we see an open bracket we add to the related stack
+            if b == "(" or b == "[" or b == "{":
+                stack.append(b) 
+
+            # when we see a close bracket we remove from the related stack
+            if b == ")" or b == "]" or b == "}":
+                # we should never try removing from an empty stack
+                if not stack:
+                    return False
+
+                # bracket type removed should match
+                c = stack.pop()
+                if b == ")" and c != "(":
+                    return False
+                if b == "]" and c != "[":
+                    return False
+                if b == "}" and c != "{":
+                    return False
+
+        # at the end all stacks should be empty
+        return not stack
