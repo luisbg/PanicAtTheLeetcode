88ea5e799df4f404214772b97000356494134037
Luis de Bethencourt <luis@debethencourt.com>
2025-12-01 16:47:09 -0500

    blind 75: 15: bit manipulation: counting bits (fancy)

--- blind_75/bit_manipulation/72_counting_bits.py
+++ blind_75/bit_manipulation/72_counting_bits.py
@@ -3,24 +3,12 @@ Given an integer n, return an array ans of length n + 1 such that for each i (0 
 """
 
 class Solution(object):
-    def numberOfBits(self, n):
-        count = 0
-        while n:
-            n &= n - 1   # clear the lowest set bit
-            count += 1
-        return count
-
     def countBits(self, n):
         """
         :type n: int
         :rtype: List[int]
         """
-        # start array
-        ans = [0]
-
-        # iterate from 1 to n
-        for x in range(1, n + 1):
-            # add number of 1s for each int
-            ans.append(self.numberOfBits(x))
-
+        ans = [0] * (n + 1)
+        for i in range(1, n + 1):
+            ans[i] = ans[i & (i - 1)] + 1
         return ans
