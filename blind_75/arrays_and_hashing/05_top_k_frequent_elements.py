faff7ee404e86d6b764092388cc8df8757fccb7e
Luis de Bethencourt <luis@debethencourt.com>
2025-12-02 15:46:27 -0500

    blind 75: 21: arrays: top k frequent elements (k-sized heap)

--- blind_75/arrays_and_hashing/05_top_k_frequent_elements.py
+++ blind_75/arrays_and_hashing/05_top_k_frequent_elements.py
@@ -29,8 +29,42 @@ class Solution(object):
 
         # make a list sized k of top seen elements
         ans = []
-        for _ in range(0, k):
+        for _ in range(k):
             tmp = heapq.heappop(h)
             ans.append(tmp[1])
+
+        return ans
+
+    # Use a k-sized heap to store less
+    def betterMemoryTopKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        # create a dictionary
+        count = {}
+
+        # track counts of each number in nums
+        # in negative since Python's heap is minHeap
+        for n in nums:
+            if n in count:
+                count[n] += 1
+            else:
+                count[n] = 1
+
+        # convert dictionary to tuple heap
+        h = []
+        for key in count:
+            heapq.heappush(h, (count[key], key))
+
+            # keep heap size at most k
+            if len(h) > k:
+                heapq.heappop(h) # remove smallest frequency
+
+        # make a list from k sized heap
+        ans = []
+        for (count, num) in h:
+            ans.append(num)
 
         return ans
