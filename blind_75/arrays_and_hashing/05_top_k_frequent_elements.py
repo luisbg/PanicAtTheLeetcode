69b4b7da39c6a0d092024e9aadc91bd679174111
Luis de Bethencourt <luis@debethencourt.com>
2025-12-02 15:40:07 -0500

    blind 75: 21: arrays: top k frequent elements

---
+++ blind_75/arrays_and_hashing/05_top_k_frequent_elements.py
@@ -0,0 +1,36 @@
+"""
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+"""
+
+import heapq
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        # create a dictionary
+        count = {}
+
+        # track counts of each number in nums
+        # in negative since Python's heap is minHeap
+        for n in nums:
+            if n in count:
+                count[n] -= 1
+            else:
+                count[n] = -1
+
+        # convert dictionary to tuple heap
+        h = []
+        for key in count:
+            heapq.heappush(h, (count[key], key))
+
+        # make a list sized k of top seen elements
+        ans = []
+        for _ in range(0, k):
+            tmp = heapq.heappop(h)
+            ans.append(tmp[1])
+
+        return ans
