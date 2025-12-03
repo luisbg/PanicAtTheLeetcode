e3d8d78f3044f633c978f5d48a0325ddd94b723e
Luis de Bethencourt <luis@debethencourt.com>
2025-12-03 14:54:31 -0500

    blind 75: 27: sliding windows: longest substring without repeating characters (optimal)

--- blind_75/sliding_window/13_longest_substring_without_repeating_chars.py
+++ blind_75/sliding_window/13_longest_substring_without_repeating_chars.py
@@ -3,7 +3,7 @@ Given a string s, find the length of the longest without duplicate characters.
 """
 
 class Solution(object):
-    def lengthOfLongestSubstring(self, s):
+    def slowLengthOfLongestSubstring(self, s):
         """
         :type s: str
         :rtype: int
@@ -28,5 +28,32 @@ class Solution(object):
             # start always needs to be left or end
             if end <= start:
                 end += 1
+
+        return longest
+
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # two pointers
+        start = 0
+        end = 0
+        longest = 0
+        dupes = set()
+
+        # run until end of substring is end of string
+        while end < len(s):
+            # use set to check for duplicates
+            if s[end] not in dupes:
+                dupes.add(s[end])
+                longest = max(longest, end - start + 1)
+
+                # if no duplicates move end
+                end += 1
+            else:
+                dupes.remove(s[start])
+                # we have duplicates, move start
+                start += 1
 
         return longest
