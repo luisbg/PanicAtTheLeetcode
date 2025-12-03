faee2c63d04b92a7101bb1356036b48e61d4e272
Luis de Bethencourt <luis@debethencourt.com>
2025-12-03 11:42:56 -0500

    blind 75: 24: arrays: longest consecutive sequence (heavy)

--- blind_75/arrays_and_hashing/08_longest_consecutive_sequence.py
+++ blind_75/arrays_and_hashing/08_longest_consecutive_sequence.py
@@ -27,3 +27,35 @@ class Solution(object):
                 longest = seq
 
         return longest
+
+    def heavyLongestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # edge case of an empty list
+        if len(nums) == 0:
+            return 0
+
+        # add all nums to a set to have O(1) checks
+        num_set = set(nums)
+
+        # get biggest
+        biggest = max(nums)
+        # get smallest
+        smallest = min(nums)
+
+        # run from smallest to biggest
+        # count sequences that exist
+        seq = 0
+        longest = 0
+        for i in range(smallest, biggest + 1):
+            if i in num_set:
+                seq += 1
+            else:
+                seq = 0
+
+            if seq > longest:
+                longest = seq
+
+        return longest
