3006aecb2bd91b9a8018759b1b9f1cd989b44b46
Luis de Bethencourt <luis@debethencourt.com>
2025-12-04 11:59:18 -0500

    blind 75: 31: linked list: reorder list (O(1) space)

--- blind_75/linked_lists/22_reorder_list.py
+++ blind_75/linked_lists/22_reorder_list.py
@@ -16,7 +16,7 @@ You may not modify the values in the list's nodes. Only nodes themselves may be 
 #         self.val = val
 #         self.next = next
 class Solution(object):
-    def reorderList(self, head):
+    def heavyReorderList(self, head):
         """
         :type head: Optional[ListNode]
         :rtype: None Do not return anything, modify head in-place instead.
@@ -46,3 +46,44 @@ class Solution(object):
         node_arr[start].next = None
 
         return head
+
+    def reorderList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: None Do not return anything, modify head in-place instead.
+        """
+        if not head or not head.next or not head.next.next:
+            return  # lists of length 0, 1, 2 are already "reordered"
+
+        # Find the middle (end of first half)
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Reverse the second half
+        prev = None
+        curr = slow.next
+        slow.next = None  # cut first half from second
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        # prev is now head of reversed second half
+        second = prev
+        first = head
+
+        # Merge the two halves
+        while second:
+            tmp1 = first.next
+            tmp2 = second.next
+
+            first.next = second
+            second.next = tmp1
+
+            first = tmp1
+            second = tmp2
