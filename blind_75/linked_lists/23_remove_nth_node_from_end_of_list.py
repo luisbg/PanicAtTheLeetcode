b171aa8b339940732e8c17c698f2dfc2919f3e9b
Luis de Bethencourt <luis@debethencourt.com>
2025-12-05 16:08:35 -0500

    blind 75: 39: linked lists: remove nth node from end of list (one pass)

--- blind_75/linked_lists/23_remove_nth_node_from_end_of_list.py
+++ blind_75/linked_lists/23_remove_nth_node_from_end_of_list.py
@@ -8,7 +8,7 @@ Given the head of a linked list, remove the nth node from the end of the list an
 #         self.val = val
 #         self.next = next
 class Solution(object):
-    def removeNthFromEnd(self, head, n):
+    def onePassRemoveNthFromEnd(self, head, n):
         """
         :type head: Optional[ListNode]
         :type n: int
@@ -35,5 +35,35 @@ class Solution(object):
                 break
             curr = curr.next
             idx +=1
+
+        return head
+
+    def twoPassRemoveNthFromEnd(self, head, n):
+        """
+        :type head: Optional[ListNode]
+        :type n: int
+        :rtype: Optional[ListNode]
+        """
+        # run through the list with two pointers
+        # faster runs from the beginning
+        # slower runs n behind
+        faster = head
+        slower = head
+
+        # move faster n steps ahead
+        for _ in range(n):
+            faster = faster.next
+
+        # if faster is None, we are removing the head
+        if not faster:
+            return head.next
+
+        # move both until faster is at the last node
+        while faster.next:
+            faster = faster.next
+            slower = slower.next
+
+        # now slower is at the place to remove
+        slower.next = slower.next.next
 
         return head
