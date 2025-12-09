d31c063b7a2a5b95cab9ee3e280dd21897a11a73
Luis de Bethencourt <luis@debethencourt.com>
2025-12-09 11:11:06 -0500

    blind 75: 43: linked lists: merge k sorted lists

---
+++ blind_75/linked_lists/24_merge_k_sorted_lists.py
@@ -0,0 +1,54 @@
+"""
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[Optional[ListNode]]
+        :rtype: Optional[ListNode]
+        """
+        # start a new list
+        ans = None
+        tail = None
+
+        # run through every list until all are empty
+        while True:
+            # check for all lists being empty
+            done = True
+            for head in lists:
+                if head:
+                    done = False
+
+            if done:
+                break
+
+            smallest = 10001
+            s_idx = -1
+            # check all heads to find the smallest one
+            for i, head in enumerate(lists):
+                if head and head.val:
+                    if head.val < smallest:
+                        smallest = head.val
+                        s_idx = i
+
+            # copy smallest head to answer
+            new_node = ListNode(smallest)
+            if tail:
+                tail.next = new_node
+                tail = new_node
+            else:
+                tail = new_node
+                ans = new_node
+
+            # remove smallest head from list
+            lists[s_idx] = lists[s_idx].next
+
+        return ans
